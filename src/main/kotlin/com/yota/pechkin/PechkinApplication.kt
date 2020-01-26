1e96cfc6ca67bd1bd8517ec82ef2db487cd46b45
Muchnik Andrey <muchnik.ak@gmail.com>
2020-01-26 22:37:01 +0300

    Added token

--- src/main/kotlin/com/yota/pechkin/PechkinApplication.kt
+++ src/main/kotlin/com/yota/pechkin/PechkinApplication.kt
@@ -6,11 +6,9 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.http.ResponseEntity.status
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.lang.System.currentTimeMillis
+import java.util.*
 
 @SpringBootApplication
 class PechkinApplication
@@ -32,6 +30,8 @@ data class Address(val id: Long, val street: String, val houseColor: String)
 
 data class Response(val message: String)
 
+data class Token(val token: String)
+
 data class User(
         val id: Long,
         val firstName: String,
@@ -41,12 +41,16 @@ data class User(
 )
 
 @RestController
+@RequestMapping("/api/v1/")
 class PechkinController {
-    @PostMapping("/api/v1/deliver")
+    @PostMapping("/deliver")
     fun receive(@RequestBody message: YotaMessage): ResponseEntity<*> = when {
-        message.token.isNullOrBlank()     -> status(HttpStatus.BAD_REQUEST).body(Response("Требуется особо секретный token"))
+        message.token.isNullOrBlank()     -> status(HttpStatus.BAD_REQUEST).body(Response("Требуется особо секретный token, можно получить с помощью запроса GET /api/v1/token"))
         message.addresses.isNullOrEmpty() -> status(HttpStatus.BAD_REQUEST).body(Response("Куда же я доставлять посылочку буду? Где адреса?"))
         currentTimeMillis() % 2 == 0L     -> status(HttpStatus.INTERNAL_SERVER_ERROR).body(Response("Извини, устал педали крутить..."))
         else                              -> ok(Response("Доставим мы посылочку вашему мальчику ${message.to.firstName}, не волнуйтесь!"))
     }
+
+    @GetMapping("/token")
+    fun token() = Token(UUID.randomUUID().toString())
 }
